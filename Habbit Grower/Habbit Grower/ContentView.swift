40254f94ec535a1f4294a02b74a401bcd0a96c63
Wen Zhang <wz34@rice.edu>
2019-09-20 23:27:30 -0500

    Empty Project!

---
+++ Habbit Grower/Habbit Grower/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habbit Grower
+//
+//  Created by Wendy Zhang on 9/20/19.
+//  Copyright © 2019 Wendy Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habbit Grower/Habbit GrowerTests/Habbit_GrowerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Habbit_GrowerTests.swift
+//  Habbit GrowerTests
+//
+//  Created by Wendy Zhang on 9/20/19.
+//  Copyright © 2019 Wendy Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import Habbit_Grower
+
+class Habbit_GrowerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
